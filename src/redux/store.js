c78e126d0bcbe2c0da6d1e3854bb79c0fbfa76ad
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-15 15:12:29 +0100

    configure redux store

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+
+export default configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(createLogger()),
+});
