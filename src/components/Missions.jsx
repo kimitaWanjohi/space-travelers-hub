548ebaa22e99258e8eeb9ab6af5c555462a91699
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-16 10:32:25 +0100

    Fetch mission in component

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,26 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchMissions } from "../redux/missions/slice";
+
 export default function Missions() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  const loading = useSelector((state) => state.missions.loading);
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
   return (
     <div>
-      <h1>Missions</h1>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        missions.map((mission) => (
+          <div key={mission.mission_id}>
+            <h1>{mission.mission_name}</h1>
+            <p>{mission.description}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
