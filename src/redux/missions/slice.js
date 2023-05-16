3eb84a63e94e72ed6dabb63083b3c38695c3a657
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-16 10:29:49 +0100

    Add asyncthunk to fetch missions.

--- src/redux/missions/slice.js
+++ src/redux/missions/slice.js
@@ -1,13 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getMissions } from '../../api/missions';
 
 const initialState = {
   missions: [],
+  status: 'idle',
 };
+
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async () => {
+    const response = await getMissions();
+    return response;
+  },
+);
 
 export const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.missions = action.payload;
+      });
+  }
 });
 
 export default missionsSlice.reducer;
