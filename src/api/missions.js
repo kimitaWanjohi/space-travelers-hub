57a49bcefe866cfe13a122d570ee4e8885fedf5b
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-18 07:04:04 +0100

    Replace axios with fetch

--- src/api/missions.js
+++ src/api/missions.js
@@ -1,9 +1,8 @@
-import axios from "axios";
 import { MISSIONS_URL } from "../consts";
 
 export const getMissions = async () => {
-  const response = await axios.get(MISSIONS_URL);
-  return response.data;
+  const response = await fetch(MISSIONS_URL);
+  return response.json();
 };
 
 export const missions = "missions";
